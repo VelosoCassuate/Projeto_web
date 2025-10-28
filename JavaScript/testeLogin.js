24f5dca0ce2ae3f39c492f7b2f685635d3ba7119
VelosoCassuate <velosocassuaty@gmail.com>
2025-10-28 15:31:26 +0100

    nova pagina de adm
    fiz algumas alteracoes e coloquei pagina para o adm

---
+++ JavaScript/testeLogin.js
@@ -0,0 +1,17 @@
+
+const user = localStorage.getItem("user")
+const chave = localStorage.getItem("chave")
+
+function enviarMensagem() {
+    if (user == null || chave == null)
+        window.open('./../paginas/login.html', '_self');
+    // alert(user + "  " + chave)
+}
+
+
+const form = document.getElementById('suporte')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    enviarMensagem();
+})
